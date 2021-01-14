10699ecac5c37e39c475d2c12283b1a7ef466574
JamieDoyle74Cap <jamie.doyle@capita.com>
2021-01-14 15:53:27 +0000

    [M3] Simple INT test looking for a status_code == 200

---
+++ todo-app/test_integration.py
@@ -0,0 +1,53 @@
+import pytest
+import requests
+from dotenv import find_dotenv, load_dotenv
+from app import create_app
+
+class mockup(object):
+    def __init__(self,json):
+        self.responseJSON = json
+    def json(self):
+        return self.responseJSON
+
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version
+    file_path = find_dotenv('todo-app/.env.test')
+    load_dotenv(file_path, override=True)
+    # Create the new app.
+    test_app = create_app()
+    # Use the app to create a test_client that can be used in our tests.
+    
+    with test_app.test_client() as client:
+        yield client
+
+
+def test_index_page(monkeypatch, client):
+    # Arrange
+    def mockget(url, params):
+        return mockup(
+            [
+                {
+                    "name": "test-item-1",
+                    "id": "test-id-1",
+                    "desc": "test-description-1",
+                    "due": "2020-12-25T12:00:00.000000Z",
+                    "dateLastActivity": "2020-10-25T12:00:00.000000Z",
+                    "idList": "test-todo-list-id"
+                },
+                {
+                    "name": "test-item-2",
+                    "id": "test-id-2",
+                    "desc": "test-description-2",
+                    "due": "2020-12-26T12:00:00.000000Z",
+                    "dateLastActivity": "2020-10-26T12:00:00.000000Z",
+                    "idList": "test-doing-list-id"
+                }
+            ])
+    monkeypatch.setattr(requests, "get", mockget)
+    # Act
+    response = client.get('/')
+    # Assert
+    assert response.status_code == 200
+
+
